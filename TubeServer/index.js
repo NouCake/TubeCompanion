b6ea0e44211f1343e9f60168d507464d75801195
NouCake <KevinFischer2010@gmail.com>
2019-01-14 22:07:46 +0100

    npm init
    added modules:
    -express
    -socket.io

---
+++ TubeServer/index.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(12012, function(){
+    console.log('Listening on Port 12012');
+});
